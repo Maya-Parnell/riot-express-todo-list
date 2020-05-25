fc5676f9420790d9deb408c38420493c75998335
Maya-Parnell <mayaparnell@gmail.com>
2020-05-25 12:04:10 -0400

    auto

--- deploy.js
+++ deploy.js
@@ -85,9 +85,9 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '00.00.00.00',
+      host: '34.204.101.159',
       username: 'ubuntu',
-      privateKey: 'hs-key.pem'
+      privateKey: 'todo-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
